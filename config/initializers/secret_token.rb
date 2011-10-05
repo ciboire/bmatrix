6e040f95ccdf980941da4e755956fdedd750452d
Brian Thorndyke <brian@thorndyke.org>
2011-10-05 11:45:05 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bmatrix::Application.config.secret_token = '203e890d22e9fa3590bc1b291b2b8432866cb718843a6907e2f3990c9c07c047a58b851d7b4679c95b6381e59f5d7fc221c9ae35a729ae19c8d62a96034108af'
